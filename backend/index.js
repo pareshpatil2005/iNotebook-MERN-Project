d42887add58c292f6ee53484ed8a89fd3b76688a
Paresh Patil <patilparesh874@gmail.com>
2025-06-26 08:31:15 +0530

    Lecture 49 Completed

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,20 @@ const express = require('express');
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello Paresh!')
-})
+app.use(express.json());     // middleware
+
+// app.get('/', (req, res) => {
+//     res.send('Hello Paresh!')
+//   })
+
+
+// Available Routes
+  
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
